adce40b1e40fdef0c7f0243d7b82e73e111bc6b2
Nicky <nick.bauters@me.com>
2023-12-14 21:45:03 +0100

    issue#1: test: Catch errors while mapping and change them into the MappingException

--- src/main/java/be/archilios/open/domainmapper/DomainMapper.java
+++ src/main/java/be/archilios/open/domainmapper/DomainMapper.java
@@ -1,5 +1,15 @@
 package be.archilios.open.domainmapper;
 
+import be.archilios.open.domainmapper.exceptions.MappingException;
+
 public class DomainMapper {
+    
+    public <T, V> T map(V source, Class<T> targetClass) {
+        try {
+            return tryMapping(source, targetClass);
+        } catch (Exception e) {
+            throw new MappingException("Could not map " + source.getClass().getSimpleName() + " to " + targetClass.getSimpleName(), e);
+        }
+    }
 
 }
