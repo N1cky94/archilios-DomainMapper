31a2f23356a35923c99f61535691823bd358b861
Nicky <nick.bauters@me.com>
2023-12-15 13:16:21 +0100

    issue#1: feat: add mapping logic for simple Pojo's

--- src/main/java/be/archilios/open/domainmapper/DomainMapper.java
+++ src/main/java/be/archilios/open/domainmapper/DomainMapper.java
@@ -2,14 +2,44 @@ package be.archilios.open.domainmapper;
 
 import be.archilios.open.domainmapper.exceptions.MappingException;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class DomainMapper {
     
     public <T, V> T map(V source, Class<T> targetClass) {
         try {
-            return tryMapping(source, targetClass);
+            return tryMap(source, targetClass);
         } catch (Exception e) {
             throw new MappingException("Could not map " + source.getClass().getSimpleName() + " to " + targetClass.getSimpleName(), e);
         }
+    }
+    
+    private <T, V> T tryMap(V Source, Class<T> targetClass) throws Exception {
+        //todo: map the source to the target untill the tests succeed
+        //   1. [X] Get a returnable result
+        //   2. [X] Get the fields of the source
+        //   3. [X] Get the fields of the target
+        //   4. [X] Loop over the source fields and check which ones are present with the same name in the target
+        //   5. [X] For all fields that can be found in both, copy the value from the source to the target
+        
+        T result = targetClass.getDeclaredConstructor().newInstance();
+        
+        Field[] sourceFields = Source.getClass().getDeclaredFields();
+        Field[] targetFields = targetClass.getDeclaredFields();
+        
+        for (Field sourceField : sourceFields) {
+            for (Field targetField : targetFields) {
+                if (sourceField.getName().equals(targetField.getName())) {
+                    sourceField.setAccessible(true);
+                    targetField.setAccessible(true);
+                    targetField.set(result, sourceField.get(Source));
+                }
+            }
+        }
+        
+        return result;
     }
 
 }
